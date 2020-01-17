32365b8a11004b07cb38270f1ed44a9abd0f0cda
yutveg <kodi.0n31l@gmail.com>
2020-01-17 09:27:54 -0800

    CRUD for actions and projects finished

--- projects/router.js
+++ projects/router.js
@@ -58,54 +58,73 @@ router.put("/:id", validateProjectId, (req, res) => {
 
 // Actions CRUD
 
-router.post("/:id/actions", (req, res) => {
-  Actions.insert({ project_id: req.params.id, description: req.body })
-    .then(result => {
-      res.status(200).json({ message: "Creation was successful." });
+router.post(
+  "/:id/actions",
+  validateProjectId,
+  validateActionPost,
+  (req, res) => {
+    Actions.insert({
+      project_id: req.params.id,
+      description: req.body.description,
+      notes: req.body.notes
     })
-    .catch(err => {
-      res.status(500).json({ error: "Something went wrong." });
-    });
-});
+      .then(result => {
+        res.status(200).json({ message: "Creation was successful." });
+      })
+      .catch(err => {
+        res.status(500).json({ error: "Something went wrong." });
+      });
+  }
+);
 
-router.get("/:id/actions", (req, res) => {
+router.get("/:id/actions", validateProjectId, (req, res) => {
   Projects.getProjectActions(req.params.id)
     .then(result => {
-      res.status(200).json({ message: "Retrieval was successful." });
+      res.status(200).json(result);
     })
     .catch(err => {
       res.status(500).json({ error: "Something went wrong." });
     });
 });
 
-router.get("/:id/actions/:actionid", (req, res) => {
-  Projects.getProjectActions(req.params.id)
-    .filter(item => item.id === req.params.actionid)
-    .then(result => {
-      res.status(200).json({ message: "Retrieval was successful." });
-    })
-    .catch(err => {
-      res.status(500).json({ error: "Something went wrong." });
-    });
-});
+router.get(
+  "/:id/actions/:actionid",
+  validateProjectId,
+  validateActionId,
+  (req, res) => {
+    Projects.getProjectActions(req.params.id)
+      .then(result => {
+        res.status(200).send(result[req.params.actionid - 1]);
+      })
+      .catch(err => {
+        res.status(500).json({ error: "Something went wrong." });
+      });
+  }
+);
 
-router.put("/:id/actions/:actionid", (req, res) => {
-  Actions.update(req.params.actionid, {
-    project_id: req.params.id,
-    description: req.body
-  })
-    .then(result => {
-      res.status(200).json({ message: "Update was successful." });
+router.put(
+  "/:id/actions/:actionid",
+  validateProjectId,
+  validateActionId,
+  (req, res) => {
+    Actions.update(req.params.actionid, {
+      project_id: req.params.id,
+      description: req.body.description,
+      notes: req.body.notes
     })
-    .catch(err => {
-      res.status(500).json({ error: "Something went wrong." });
-    });
-});
+      .then(result => {
+        res.status(200).json({ message: "Update was successful." });
+      })
+      .catch(err => {
+        res.status(500).json({ error: "Something went wrong." });
+      });
+  }
+);
 
-router.put("/:id/actions/:actionid", (req, res) => {
-  Actions.delete(req.params.actionid)
+router.delete("/:id/actions/:actionid", (req, res) => {
+  Actions.remove(req.params.actionid)
     .then(result => {
-      res.status(200).json({ message: "Update was successful." });
+      res.status(200).json({ message: "Deletion was successful." });
     })
     .catch(err => {
       res.status(500).json({ error: "Something went wrong." });
@@ -122,10 +141,26 @@ function validateProjectId(req, res, next) {
   });
 }
 
+function validateActionId(req, res, next) {
+  Actions.get(req.params.actionid).then(result => {
+    result === null
+      ? res.status(400).json({ error: "action not found" })
+      : next();
+  });
+}
+
 function validateProjectPost(req, res, next) {
   if (Object.keys(req.body).length === 0) {
     res.status(400).json({ error: "No content for post detected" });
   } else if (!req.body.name || !req.body.description) {
+    res.status(400).json({ error: "Please include all required fields." });
+  } else next();
+}
+
+function validateActionPost(req, res, next) {
+  if (Object.keys(req.body).length === 0) {
+    res.status(400).json({ error: "No content for post detected" });
+  } else if (!req.body.description || !req.body.notes) {
     res.status(400).json({ error: "Please include all required fields." });
   } else next();
 }
